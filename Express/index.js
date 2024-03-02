787ceb9d109bcab411534e674f56a1eb6b477ae4
Sansão Maposse <121869087+SansaoMaposse@users.noreply.github.com>
2024-03-02 20:23:16 +0200

    Update index.js

--- Express/index.js
+++ Express/index.js
@@ -4,6 +4,14 @@ const app = express();
 app.get('/', function(req, res){
     res.send('Seja bem-vindo ao App com Node.js');
 });
+
+app.get('/sobre', function(req, res){
+    res.send('Saiba um pouco sobre me');
+})
+
+ app.get('/blog',function(req, res){
+    res.send('Meu blog');
+})
  
 
 app.listen(8090, function(){
