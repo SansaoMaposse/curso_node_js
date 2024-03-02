7d016b1d61665001f9cf981dce94ba9a29bbf803
Sansão Maposse <121869087+SansaoMaposse@users.noreply.github.com>
2024-03-02 20:16:06 +0200

    Update index.js
    rotas no node js

--- Express/index.js
+++ Express/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/', function(req, res){
+    res.send('Seja bem-vindo ao App com Node.js');
+});
  
 
 app.listen(8090, function(){
