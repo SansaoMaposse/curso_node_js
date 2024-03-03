14b6607a3424c876cf990e6cade427282cc73eba
Sansão Maposse <type>
2024-03-03 23:28:11 +0200

    Update test.js

--- Express/test.js
+++ Express/test.js
@@ -1,1 +1,16 @@
 const Sequilize = require('sequelize');
+const sequilize = new Sequilize('test', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const Postagem = sequilize.define('postagens',{
+    titulo:{
+        type: Sequilize.STRING,
+    },
+    conteudo:{
+        type: Sequilize.TEXT
+    }
+});
+
+Postagem.sync({force: true})
